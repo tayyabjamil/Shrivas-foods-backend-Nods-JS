68d90dbf81697920d2afae77781bd4ca874be136
unknown <tayyabjamil777@gmail.com>
2020-11-30 20:09:20 +0500

    make it better

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
 const Grid = require('gridfs-stream');
-const app = require('./app')
-
-
-app.listen(process.env.port ,'0.0.0.0' , ()=>{
-    console.log('listening at port '+process.env.port)
-})
+const express = require('express')
+var path = require("path");
+const PORT = process.env.PORT || 5000
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 // const dbpath = 'mongodb://localhost:27017/Nodejs'
 // mongoose.connect('mongodb+srv://tayyab:tayyab@123@cluster0.wzfwv.mongodb.net/shirivasFoods', 
 // {useNewUrlParser: true ,useCreateIndex:true,useFindAndModify:false}).then(() => {
