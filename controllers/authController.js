69694ec2709305e4c042f6f2537035f28b5a97b5
shrivasa33 <74127144+shrivasa33@users.noreply.github.com>
2021-01-09 03:30:37 -0500

    matching

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,7 +31,7 @@ if(!user){
 }
 const restToken = user.createPaswordRestToken();
 await user.save({validateBeforeSave:false});
-// const resetUrl = `https://test-shrivasafoods.tk/resetPassword${restToken}`
+// const resetUrl = `http://localhost:4200/resetPassword${restToken}`
 const message = `forget password reset password at url ${restToken}`
 try {
     forgetPasswordMail(req.body.email,restToken,message);
