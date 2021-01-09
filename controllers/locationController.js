667261c27a37099154d293725c720fe8702b8588
shrivasa33 <74127144+shrivasa33@users.noreply.github.com>
2021-01-09 03:22:05 -0500

    Update the code

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -10,17 +10,17 @@ const ObjectId = mongoose.Types.ObjectId;
 exports.location = async (req, res) => {
       googleapiresponse = ""
     try {
-        googleUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${req.body.latitude},${req.body.longitude}&key=AIzaSyAYJvPnMzFkvkeka7kw_aV4Pjn3TeeACv8`
+        googleUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${req.body.latitude},${req.body.longitude}&key=AIzaSyCYbrTDQwp2ZOQbInLEUyjpJHswGEDJaL4`
         console.log(googleUrl)
-      request(googleUrl, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
+        request(googleUrl, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
                 googleapiresponse = body
             }
-          console.log(googleapiresponse)
-       });
+            console.log(googleapiresponse)
+        });
         res.status(200).json({
-            googleapiresponse: googleapiresponse.results,
-        })      
+            googleapiresponse: googleapiresponse
+    })      
     } catch (error) {
         res.status(404).json({
             status:' No order found',
